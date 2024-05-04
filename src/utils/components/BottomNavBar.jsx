fefa1c2da362c5ce05a43fe28440fc9d0b7e22d7
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-05-04 22:59:06 +0100

    Update BottomNavBar.jsx

--- src/utils/components/BottomNavBar.jsx
+++ src/utils/components/BottomNavBar.jsx
@@ -2,6 +2,8 @@ import { Bell, Home1, Message, Setting } from "@iconsans/react/linear"
 import { Link } from "react-router-dom"
 
 const BottomNavBar = () => {
+    // create a fucntion to check which one is active and then set the icon color to info color and scale up
+
     return (
         <div className='w-screen h-20 bg-white border-t-2 border-grey fixed bottom-0 left-0 right-0 flex flex-row p-3 gap-3'>
 
